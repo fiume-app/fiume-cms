bc71b0aa67461e231e49ef75ef1d35b0080616a4
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-09-16 11:58:49 +0530

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,19 @@
 import { buildConfig } from 'payload/config';
 import path from 'path';
-// import Examples from './collections/Examples';
-import Users from './collections/Users';
+import Admins from './collections/Admins';
+import Resources from './collections/Resources';
+import AccessControlPolicies from './collections/AccessControlPolicies';
 
 export default buildConfig({
   serverURL: 'http://localhost:3000',
   admin: {
-    user: Users.slug,
+    user: Admins.slug,
   },
   collections: [
-    Users,
+    Admins,
+    AccessControlPolicies,
+    Resources,
     // Add Collections here
-    // Examples,
   ],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
